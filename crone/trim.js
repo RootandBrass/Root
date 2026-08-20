f1c9ef549eadc37d947687a032c2e3c29049dacf
Root&Brass <bobbiejjones04@gmail.com>
2026-08-20 16:10:57 -0600

    Remove obsolete Crone trim shim

--- crone/trim.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-  const intro=document.querySelector('.intro');
-  if(!intro)return;
-  const next=intro.nextElementSibling;
-  intro.remove();
-  if(next&&next.classList.contains('rule'))next.remove();
-  const top=document.querySelector('.top');
-  if(top)top.style.marginBottom='22px';
-})();
